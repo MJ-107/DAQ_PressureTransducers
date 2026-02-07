3a7c4196e4234a89c44248f306abda12d925f6f2
Macie <Macie@MSI>
2026-02-07 16:21:12 -0500

    Organized project into folders to read/manipulate data from DAQ. Set framework to readDAQData script.

---
+++ Read DAQ Data/readDAQData.m
@@ -0,0 +1,12 @@
+% DAQ Pressure Transducer
+% Read transducer outputs ro NI 6008
+
+%% Read 2 differential analog input pairs
+% Read 2 Validyne P-17 outputs
+
+% For Up/Down Direction
+
+% For Left/Right Direction
+
+%% Read 1 single analog input
+% Read Omegadyne output
