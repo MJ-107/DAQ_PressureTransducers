4d3156b4d894c521e18bfdebf2c2f56078039596
Macie <Macie@MSI>
2026-02-07 15:59:28 -0500

    Added main. Added live plotting function to read DAQ voltages in real time.

---
+++ Live Plots/CreatePressurePlot.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = CreatePressurePlot(inputArg1,inputArg2)
+%CREATEPRESSUREPLOT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ Live Plots/CreateVoltagePlot.m
@@ -0,0 +1,26 @@
+% DAQ Pressure Transducer
+% Create and display live plot of voltage readings
+
+function CreateVoltagePlot(data, channelNames)
+
+    time = data.Time; % Get time vector
+    numberOfChanels = numel(channelNames); % Get number of input chanels
+
+    % Initialize figure
+    figure
+    hold on
+    grid on
+
+    % Cycle through input chanels and plot
+    for i = 1:1:numberOfChanels
+        plot(time, data.(channelNames{i}), "LineWidth", 1.2)
+    end
+
+    hold off
+
+    % Add axis labels
+    xlabel("Time (s)")
+    ylabel("Voltage (V)")
+    title("Live Analog Input Voltages")
+    legend(channelNames, "Interpreter", "none", "Location", "best")
+end
