99b1b9741b96dbe850f76a94c67d003ede9a498a
Macie <Macie@MSI>
2026-02-12 17:06:06 -0500

    added fcn to start daq session recording and cofigure 2 differential channels. 2 differential channels are for x2 validyne P17s (output range +/- 5V).

---
+++ Read DAQ Data/startDAQSession.m
@@ -0,0 +1,38 @@
+function session = startDAQSession(devices)
+
+%   Inputs:
+%     devices - String specifying DAQ vendor
+
+%   Outputs:
+%     session - DAQ obj
+
+    arguments
+        devices table
+    end
+
+    % Use first detected device
+    deviceID = devices.DeviceID(1);
+
+    try
+        % Create DAQ object
+        session = daq("ni");
+
+        % Channel 1 - Differential
+        ch1 = addinput(session, deviceID, "ai0", "Voltage");
+        ch1.TerminalConfig = "Differential";
+        ch1.Range = [-5 5]; % Range specified by Validyne for P17
+
+        % Channel 2 - Differential
+        ch2 = addinput(session, deviceID, "ai1", "Voltage");
+        ch2.TerminalConfig = "Differential";
+        ch2.Range = [-5 5]; % Range specified by Validyne for P17
+
+        session.Rate = 1000; % 1 kHz sampling
+
+        fprintf("Pressure DAQ session started on %s\n", deviceID);
+
+    catch ME
+        error("Failed to start DAQ session: %s", ME.message);
+    end
+
+end
--- main.m
+++ main.m
@@ -19,4 +19,8 @@ else
     fprintf("No DAQ devices detected for vendor '%s'.\n", vendor);
 end
 
-%% 
+%% Create DAQ Session
+
+  s = daq.createSession('ni');
+  %add analog channel  s.addAnalogInputChannel('ID',channel num, 'measurement type')
+  s.addAnalogInputChannel('Dev1',3, 'Voltage')
