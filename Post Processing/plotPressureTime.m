5c5c41995aa3724bc796e6effc107559b35c3ebb
Macie <Macie@MSI>
2026-03-22 12:01:41 -0400

    added voltage and pressure post processing plot

---
+++ Post Processing/plotPressureTime.m
@@ -0,0 +1,14 @@
+function plotPressureTime(logfilename)
+
+dataTable = table2array(readtable(logfilename));
+time = dataTable(:,1);
+pressure = dataTable(:,3);
+
+figure;
+plot(time, pressure, '-b', 'LineWidth', 1.5);
+xlabel('Time');
+ylabel('Pressure');
+title('Pressure vs Time');
+grid on;
+
+end
---
+++ Post Processing/plotVoltageTime.m
@@ -0,0 +1,14 @@
+function plotVoltageTime(logfilename)
+
+dataTable = table2array(readtable(logfilename));
+time = dataTable(:,1);
+voltage = dataTable(:,2);
+
+figure;
+plot(time, voltage, '-b', 'LineWidth', 1.5);
+xlabel('Time');
+ylabel('Voltage');
+title('Voltage vs Time');
+grid on;
+
+end
